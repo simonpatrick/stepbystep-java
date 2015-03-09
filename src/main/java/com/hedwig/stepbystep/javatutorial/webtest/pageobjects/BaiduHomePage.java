9fa019ed1ab04bf830b2c244123623ba7e9929dd
simonpatrick <simonpatrick@163.com>
2015-03-09 14:40:23 +0800

    init home page

---
+++ src/main/java/com/hedwig/stepbystep/javatutorial/webtest/pageobjects/BaiduHomePage.java
@@ -0,0 +1,15 @@
+package com.hedwig.stepbystep.javatutorial.webtest.pageobjects;
+
+import org.openqa.selenium.WebElement;
+
+/**
+ * Created by patrick on 15/3/9.
+ *
+ * @version $Id$
+ */
+
+
+public class BaiduHomePage {
+
+    private WebElement keyword;
+}
